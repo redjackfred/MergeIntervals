b1eb8053cd6428275f2889359fac0ae7dff00677
Pei-Wen Tang (Vincent) <redjackfred@gmail.com>
2024-02-03 16:46:25 -0800

    Bug fix with out of bound

--- src/Leetcode56.java
+++ src/Leetcode56.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Leetcode56 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Leetcode56 {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] a, int[] b){
+                return a[0] - b[0];
+            }
+        });
+
+        List<int[]> ansList = new ArrayList<int[]>();
+        int start = intervals[0][0],
+                end = intervals[0][1];
+        int[] interval = new int[2];
+        for(int i = 0; i < intervals.length; i++){
+            if(intervals[i][0] == start){
+                end = Math.max(end, intervals[i][1]);
+            }else{
+                if(intervals[i][0] <= end){
+                    end = Math.max(end, intervals[i][1]);
+                }else{
+                    ansList.add(new int[]{start, end});
+                    start = intervals[i][0];
+                    end = intervals[i][1];
+                }
+            }
+        }
+        ansList.add(new int[]{start, end});
+
+        return ansList.toArray(new int[ansList.size()][]);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Leetcode56 leetcode56 = new Leetcode56();
+        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+        int[][] newIntervals = leetcode56.merge(intervals);
+        for(int i = 0; i < newIntervals.length; i++) {
+            System.out.println(Arrays.toString(newIntervals[i]));
+        }
     }
 }
